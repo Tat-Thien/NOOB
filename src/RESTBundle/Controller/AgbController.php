2c6b6a9108e82e297b55c57718b6537326ff3b01
David Soto Setzke <david.soto@web.de>
2016-06-14 10:53:39 +0200

    added auth to agbs

--- src/RESTBundle/Controller/AgbController.php
+++ src/RESTBundle/Controller/AgbController.php
@@ -2,6 +2,8 @@
 
 namespace RESTBundle\Controller;
 use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Request\ParamFetcherInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use FOS\RestBundle\Controller\Annotations as REST;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
@@ -13,27 +15,40 @@ use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 class AgbController extends FOSRestController
 {
 
+    private function checkAuthentication(ParamFetcherInterface $paramFetcher)
+    {
+        $providedAccessToken = $paramFetcher->get('access_token');
+        $securedAccessToken = $this->getParameter('access_token');
+        if ($providedAccessToken != $securedAccessToken) {
+            throw new AccessDeniedHttpException();
+        }
+    }
+
     /**
+     * @REST\QueryParam(name="access_token", allowBlank=false)
      * @ApiDoc(
      *  resource=true,
      *  description="Get all AGB data"
      * )
      */
-    public function cgetAction()
+    public function cgetAction(ParamFetcherInterface $paramFetcher)
     {
+        $this->checkAuthentication($paramFetcher);
         $em = $this->getDoctrine()->getManager();
         $entities = $em->getRepository('AIESECGermany\EntityBundle\Entity\AGB')->findAll();
         return $entities;
     }
 
     /**
+     * @REST\QueryParam(name="access_token", allowBlank=false)
      * @ApiDoc(
      *  resource=true,
      *  description="Get AGB data"
      * )
      */
-    public function getAction($agbID)
+    public function getAction(ParamFetcherInterface $paramFetcher, $agbID)
     {
+        $this->checkAuthentication($paramFetcher);
         $em = $this->getDoctrine()->getManager();
         $entity = $em->getRepository('AIESECGermany\EntityBundle\Entity\AGB')->findOneById($agbID);
         if ($entity) {
