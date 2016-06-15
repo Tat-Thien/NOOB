7daa273990ca230ccc23ef53b0d8f2873b008e8b
David Soto Setzke <david.soto@web.de>
2016-06-15 17:38:31 +0200

    added fininformation form

---
+++ src/RESTBundle/Form/FinanceInformationType.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace RESTBundle\Form;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FinanceInformationType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('exactEPAccountNumber')
+            ->add('paymentMode')
+            ->add('amountOfInpayment', NumberType::class)
+            ->add('inpaymentBooked', CheckboxType::class)
+            ->add('amountOfRaisingFee', NumberType::class)
+            ->add('amountOfICCFee', NumberType::class)
+            ->add('amountOfMatchingFee', NumberType::class)
+            ->add('whsFee', NumberType::class)
+            ->add('amountOfRefunding', NumberType::class)
+            ->add('refundBooked', CheckboxType::class)
+            ->add('amountOfRetention', NumberType::class)
+            ->add('retentionBooked', CheckboxType::class)
+            ->add('exactEpAccountInBalance', CheckboxType::class)
+            ->add('finComments')
+            ->add('iSOS', CheckboxType::class)
+            ->add('amountOfISOSFee', NumberType::class)
+            ->add('dateOfInpayment', DateType::class, array(
+                'widget' => 'single_text'
+            ))
+            ->add('raisingFeeBooked', CheckboxType::class)
+            ->add('iccFeeBooked', CheckboxType::class)
+            ->add('matchingFeeBooked', CheckboxType::class)
+            ->add('pedsFeeBooked', CheckboxType::class)
+            ->add('reasonForRefunding')
+            ->add('otherReasonsForRefunding')
+            ->add('reasonForRetention')
+            ->add('otherReasonsForRetention')
+            ->add('calculatedBalance', NumberType::class)
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AIESECGermany\EntityBundle\Entity\FinanceInformation',
+            'csrf_protection' => false
+        ));
+    }
+
+    public function getName()
+    {
+        return "financeInformation";
+    }
+}
