341b0929dc40a29a946b90fe55387b0c5d57c55b
David Soto Setzke <david.soto@web.de>
2016-06-22 09:47:25 +0200

    added hateoas links for exchanges in person

---
+++ src/RESTBundle/Controller/PersonExchangeRelationProvider.php
@@ -0,0 +1,27 @@
+<?php
+namespace RESTBundle\Controller;
+
+use AIESECGermany\EntityBundle\Entity\Person;
+use Hateoas\Configuration\Metadata\ClassMetadataInterface;
+use Hateoas\Configuration as Hateoas;
+
+class PersonExchangeRelationProvider
+{
+
+    public static function getRelations(Person $person, ClassMetadataInterface $classMetadata)
+    {
+        $relations = array();
+        foreach ($person->getExchanges() as $exchange) {
+            $relation = new Hateoas\Relation(
+                'exchanges',
+                new Hateoas\Route(
+                    'get_people_exchanges',
+                    array('personID' => $person->getId(),
+                        'exchangeID' => $exchange->getId())
+                )
+            );
+            array_push($relations, $relation);
+        }
+        return $relations;
+    }
+}
