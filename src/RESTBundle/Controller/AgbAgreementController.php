d0cf328f8750ba1d25b46acf482e5ca3359bf727
David Soto Setzke <david.soto@web.de>
2016-06-23 16:13:27 +0200

    implemented new AGB agreement endpoint

---
+++ src/RESTBundle/Controller/AgbAgreementController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace RESTBundle\Controller;
+
+use AIESECGermany\EntityBundle\Entity\AGBAgreement;
+use Doctrine\ORM\Query;
+use FOS\RestBundle\Request\ParamFetcher;
+use FOS\RestBundle\Request\ParamFetcherInterface;
+use RESTBundle\Form\AGBAgreementType;
+use FOS\RestBundle\Controller\Annotations as REST;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
+/**
+ * @REST\RouteResource("AGBAgreement")
+ */
+class AgbAgreementController extends RESTBundleController
+{
+
+    /**
+     * @REST\QueryParam(name="agb", description="AGB ID")
+     * @REST\QueryParam(name="exchange", description="Exchange ID")
+     * @REST\QueryParam(name="access_token", allowBlank=false)
+     * @REST\QueryParam(name="page", requirements="\d+", default="1", description="Page of the overview.")
+     * @REST\QueryParam(name="limit", requirements="\d+", default="10", description="Entities per page.")
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Get all AGB agreements",
+     *  output={"class"="RESTBundle\Form\OutgoerPreparationParticipationType", "collection"=true}
+     * )
+     */
+
+    public function cgetAction(ParamFetcherInterface $paramFetcher)
+    {
+        $this->checkAuthentication($paramFetcher);
+        $em = $this->getDoctrine()->getManager();
+        $qb = $em->createQueryBuilder();
+        $agbID = $paramFetcher->get('agb');
+        $exchangeID = $paramFetcher->get('exchange');
+        $qb->select('a')->from('AIESECGermany\EntityBundle\Entity\AGBAgreement', 'a');
+        if ($agbID) {
+            $qb->andWhere('o.agb = :agbID')->setParameter('agbID', $agbID);
+        }
+        if ($exchangeID) {
+            $qb->andWhere('r.exchange = :exchangeID')->setParameter('exchangeID', $exchangeID);
+        }
+        $query = $qb->getQuery();
+        $pagination = $this->createPaginationObject($paramFetcher, $query);
+        return $pagination;
+    }
+
+    /**
+     * @REST\QueryParam(name="access_token", allowBlank=false)
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Get AGB data",
+     *  output={"class"="RESTBundle\Form\AGBAgreementType", "collection"=true}
+     * )
+     */
+    public function getAction(ParamFetcher $paramFetcher, $agbAgreementID)
+    {
+        $this->checkAuthentication($paramFetcher);
+        $em = $this->getDoctrine()->getManager();
+        $agbAgreement = $em->getRepository('AIESECGermany\EntityBundle\Entity\AGBAgreement')->findOneById($agbAgreementID);
+        if (!$agbAgreement) {
+            throw new NotFoundHttpException();
+        }
+        return $agbAgreement;
+    }
+
+    /**
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Sign AGBs",
+     *  input="RESTBundle\Form\AGBAgreementType",
+     *  output="RESTBundle\Form\AGBAgreementType"
+     * )
+     */
+    public function postAction(ParamFetcher $paramFetcher, Request $request)
+    {
+        $agbAgreement = new AGBAgreement();
+        $form = $this->createForm(new AGBAgreementType(), $agbAgreement);
+        $form->submit($request);
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($agbAgreement);
+            $em->flush();
+            return $this->routeRedirectView('get_agbagreement', array('agbAgreementID' => $agbAgreement->getId()));
+        }
+        return array(
+            'form' => $form
+        );
+    }
+}
