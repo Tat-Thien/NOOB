ced5f11f41045c0bf0f554762e351f4be157a7de
Lukas Ehnle <lukas@Lukass-MacBook-Pro-6.local>
2016-09-20 21:38:38 +0200

    added created[from] and updated[from] filters to exchange objects

--- src/RESTBundle/Controller/ExchangeController.php
+++ src/RESTBundle/Controller/ExchangeController.php
@@ -3,6 +3,7 @@
 namespace RESTBundle\Controller;
 
 use AIESECGermany\EntityBundle\Entity\ExchangeAGB;
+use DateTime;
 use FOS\RestBundle\Controller\Annotations as REST;
 use FOS\RestBundle\Request\ParamFetcherInterface;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
@@ -18,6 +19,10 @@ class ExchangeController extends RESTBundleController
      * @REST\QueryParam(name="access_token", allowBlank=false)
      * @REST\QueryParam(name="page", requirements="\d+", default="1", description="Page of the overview.")
      * @REST\QueryParam(name="limit", requirements="\d+", default="10", description="Entities per page.")
+     * @REST\QueryParam(array=true, name="updated")
+     * @REST\QueryParam(name="updated[from]", description="Filter updated from")
+     * @REST\QueryParam(array=true, name="created")
+     * @REST\QueryParam(name="created[from]", description="Filter created from")
      * @ApiDoc(
      *  resource=true,
      *  description="Return all exchanges",
@@ -29,7 +34,16 @@ class ExchangeController extends RESTBundleController
         $this->checkAuthentication($paramFetcher);
         $em = $this->getDoctrine()->getManager();
         $qb = $em->createQueryBuilder();
-        $qb->select('e')->from('AIESECGermany\EntityBundle\Entity\Exchange', 'e');
+        $qb->select('e')
+            ->from('AIESECGermany\EntityBundle\Entity\Exchange', 'e');
+
+        $updated = $paramFetcher->get('updated');
+        $created = $paramFetcher->get('created');
+        if(isset($updated['from'])) $qb->andWhere('e.updatedAt > :updatedFrom')
+                ->setParameter('updatedFrom', DateTime::createFromFormat("!Y-m-d", $updated['from']));
+        if(isset($created['from'])) $qb->andWhere('e.createdAt > :createdFrom')
+            ->setParameter('createdFrom', DateTime::createFromFormat("!Y-m-d", $created['from']));
+
         $query = $qb->getQuery();
         $pagination = $this->createPaginationObject($paramFetcher, $query);
         return $pagination;
