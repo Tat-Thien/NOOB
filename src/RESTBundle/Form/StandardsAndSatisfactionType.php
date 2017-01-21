f877b8859b02d94310f441e6c628da7f3d32a996
Lukas Ehnle <ocjojo@users.noreply.github.com>
2017-01-21 20:34:54 +0100

    add transform to make switching to false possible

--- src/RESTBundle/Form/StandardsAndSatisfactionType.php
+++ src/RESTBundle/Form/StandardsAndSatisfactionType.php
@@ -3,6 +3,7 @@
 namespace RESTBundle\Form;
 
 use AIESECGermany\EntityBundle\Entity\StandardsAndSatisfaction;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -14,25 +15,39 @@ class StandardsAndSatisfactionType extends AbstractRESTFormType
      * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+    {   
+        $checkboxes = [
+        'visaWorkPermit',
+        'arrivalPickup',
+        'departureSupport',
+        'jobDescription',
+        'duration',
+        'workingHours',
+        'firstDayOfWork',
+        'individualGoals',
+        'insurance',
+        'accomodation',
+        'livingCosts',
+        'purpose',
+        'expectationSetting',
+        'preparation',
+        'hostFacilitatedLearning',
+        'homeFacilitatedLearning'
+        ];
+        foreach($checkboxes as $checkbox){
+            $builder->add($checkbox, CheckboxType::class);
+            $builder->get($checkbox)
+            ->addViewTransformer(new CallbackTransformer(
+                function ($normalizedFormat) {
+                    return $normalizedFormat;
+                },
+                function ($submittedFormat) {
+                    return ( $submittedFormat === 'pikachu' ) ? null : $submittedFormat;
+                }
+            ));
+        }
         $builder
-            ->add('visaWorkPermit', CheckboxType::class)
-            ->add('arrivalPickup', CheckboxType::class)
-            ->add('departureSupport', CheckboxType::class)
-            ->add('jobDescription', CheckboxType::class)
-            ->add('duration', CheckboxType::class)
-            ->add('workingHours', CheckboxType::class)
-            ->add('firstDayOfWork', CheckboxType::class)
-            ->add('individualGoals', CheckboxType::class)
-            ->add('insurance', CheckboxType::class)
-            ->add('accomodation', CheckboxType::class)
-            ->add('livingCosts', CheckboxType::class)
-            ->add('purpose', CheckboxType::class)
-            ->add('expectationSetting', CheckboxType::class)
-            ->add('preparation', CheckboxType::class)
-            ->add('hostFacilitatedLearning', CheckboxType::class)
-            ->add('homeFacilitatedLearning', CheckboxType::class)
-            ->add('comments', TextType::class)
+        ->add('comments', TextType::class)
         ;
     }
 
